954117f3fa56e66f401face3bd3650ff5b836678
nox <nox@debian>
2016-09-09 18:43:49 +0300

    Add sessions

--- run.py
+++ run.py
@@ -6,14 +6,19 @@ from sys import argv
 from datetime import date
 import sqlite3
 import flask
+import os
+import hashlib
 
 app = flask.Flask(__name__)
 
 def create_db():
-    db = sqlite3.connect('txti.db')
-    db.execute("CREATE TABLE posts (id INTEGER PRIMARY KEY, date TEXT NOT NULL, url TEXT NOT NULL, title TEXT NOT NULL, content TEXT NOT NULL, isStatic BOOLEAN NOT NULL, isPublished BOOLEAN NOT NULL)")
-    db.commit()
-    db.close()
+    if not os.path.exists('txti.db'):
+        db = sqlite3.connect('txti.db')
+        db.execute("CREATE TABLE posts (id INTEGER PRIMARY KEY, date TEXT NOT NULL, url TEXT NOT NULL, title TEXT NOT NULL, content TEXT NOT NULL, isStatic BOOLEAN NOT NULL, isPublished BOOLEAN NOT NULL)")
+        db.execute("CREATE TABLE users (user TEXT NOT NULL, password TEXT NOT NULL)")
+        db.execute("INSERT INTO users (user, password) VALUES ('admin', '')")
+        db.commit()
+        db.close()
 
 
 @app.route('/')
@@ -116,6 +121,24 @@ def delete(name):
     db.commit()
     return flask.redirect('/')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if flask.request.method == 'POST':
+        db = sqlite3.connect('txti.db')
+        c = db.cursor()
+        c.execute("SELECT user,password FROM users")
+        data = c.fetchone()
+        pass_hash = hashlib.md5()
+        pass_hash.update(flask.request.form['password'])
+        if flask.request.form['username'] == data[0] and pass_hash.hexdigest() == data[1]:
+            flask.session['logged_in'] = True
+            return flask.redirect('/write')
+    return flask.render_template('login.html', error=error)
+
+
 
 if __name__ == '__main__':
+    create_db()
+    app.config.update(SECRET_KEY='development key')
     app.run(host= 'localhost')
